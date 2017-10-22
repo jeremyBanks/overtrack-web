20751c8c182a4e81c91417b8e47699dcaa468621
Jeremy Banks <_@jeremy.ca>
2017-10-22 05:10:51 -0400

    Revert "Merge branch 'timely' into jeremyBanks"
    This reverts commit b5fd144ee6013b55ccd14b60d432ece7ce2dbddc, reversing
    changes made to 7adda71946d28077ff3d13b3c4ef7659dd41141b.

--- src/games-graph/games-graph.component.ts
+++ src/games-graph/games-graph.component.ts
@@ -144,10 +144,10 @@ export class GamesGraphComponent implements OnInit {
 
         // Graphable games from all players, flattened into a single sorted array.
         const allGames = gamesWithXs.reduce((a, b) => a.concat(b)).sort((a, b) => this.compareGamesChronologically(a.data, b.data));
-        const allTimestamps: number[] = [];
+        const allXs: number[] = [];
         for (let x = 0; x < allGames.length; x++) {
             allGames[x].x = x;
-            allTimestamps.push(+allGames[x].date);
+            allXs.push(x);
         }
 
         // Find all unique dates from all games.
@@ -266,19 +266,22 @@ export class GamesGraphComponent implements OnInit {
 
             let lastGame: FullyAnnotatedGame = null;
             for (const game of games) {
-                if (lastGame) {
-                    lineXs.push(+game.date - 1);
+                if (lastGame && (lastGame.x < game.x - 1)) {
+                    // If we're not immediately following the last game,
+                    // project its SR value forward to x - 1 so this game
+                    // only takes the typical amount of x-space (1).
+                    lineXs.push(game.x - 1);
                     lineSRs.push(lastGame.sr);
                 }
 
-                lineXs.push(+game.date);
+                lineXs.push(game.x);
                 lineSRs.push(game.sr);
 
                 let labelPrefix = '';
                 if (game.srWasUnknownReason) {
                     labelPrefix = `SR estimated (${game.srWasUnknownReason})<br>`;
                 }
-                dotXs.push(+game.date);
+                dotXs.push(game.x);
                 dotSRs.push(game.sr);
                 dotLabels.push(`${labelPrefix}${game.data.result} - ${game.data.map}`);
 
@@ -287,7 +290,7 @@ export class GamesGraphComponent implements OnInit {
 
             // Project SR line out beyond the end of the graph with latest value.
             lineSRs.push(lineSRs[lineSRs.length - 1]);
-            lineXs.push(allTimestamps[allTimestamps.length - 1]);
+            lineXs.push(allXs[allXs.length - 1] + 1024);
 
             plotlyDataLines.push({
                 showlegend: true,
@@ -339,8 +342,8 @@ export class GamesGraphComponent implements OnInit {
                         type: 'rect',
                         xref: 'x',
                         yref: 'y',
-                        x0: +game.date - 0.5,
-                        x1: +game.date + 0.5,
+                        x0: game.x - 0.5,
+                        x1: game.x + 0.5,
                         y0: minSR - 25,
                         y1: maxSR + 25,
                         line: {
@@ -381,8 +384,8 @@ export class GamesGraphComponent implements OnInit {
         plotlyLayout.showlegend = playerIndicies.length > 1;
 
         // Define limits for panning/zooming.
-        const minLeft = allTimestamps[0] - 2;
-        const maxRight = allTimestamps[allTimestamps.length - 1] + 2;
+        const minLeft = -2;
+        const maxRight = allXs[allXs.length - 1] + 2;
         const maxRange = maxRight - minLeft;
         const minRange = 2;
 
@@ -421,7 +424,8 @@ export class GamesGraphComponent implements OnInit {
                 if (enabledTraces && enabledTraces.indexOf(game.data.player) == -1) {
                     continue;
                 }
-                if (+game.date >= left && +game.date <= right) {
+
+                if (game.x >= left && game.x <= right) {
                     if (game.sr < minSR) {
                         minSR = game.sr;
                     }
@@ -462,14 +466,13 @@ export class GamesGraphComponent implements OnInit {
         };
 
         // Set the initial range to include the last 100 games.
-        const intitialLeft = allTimestamps[Math.max(allTimestamps.length - initialGamesVisible, 0)] - 0.5;
-        const initialRight = allTimestamps[allTimestamps.length - 1] + 1;
+        const intitialLeft = allXs[Math.max(allXs.length - initialGamesVisible, 0)] - 0.5;
+        const initialRight = allXs[allXs.length - 1] + 1;
         const initialLayout = getLayout(intitialLeft, initialRight, null);
         plotlyLayout.xaxis.range = initialLayout['xaxis.range'];
         plotlyLayout.yaxis.range = initialLayout['yaxis.range'];
         plotlyLayout.xaxis.ticktext = initialLayout['xaxis.ticktext'];
-        plotlyLayout.xaxis.tickvals = allDates.map(d => +d.date);
-
+        plotlyLayout.xaxis.tickvals = allDates.map(d => d.x);
         // Find our target element and let TypeScript know about the properties Plotly will add.
         const plotlyElement = document.getElementById('sr-graph') as HTMLElement & {
             on: (eventName: string, callback: (eventData: {
@@ -555,8 +558,8 @@ export class GamesGraphComponent implements OnInit {
                 games = games.slice(-eventData['last']);
             }
 
-            const left = +games[0].date - 0.5;
-            const right = +games[games.length - 1].date + 0.5;
+            const left = games[0].x - 0.5;
+            const right = games[games.length - 1].x + 0.5;
 
             Plotly.relayout(plotlyElement, {
                 source: 'constrainZoom',
